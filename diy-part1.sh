0ebb57beb1b18f2ea3b040b16f667c94ac0899c3
z826540272 <56025847+z826540272@users.noreply.github.com>
2021-01-08 06:56:21 +0000

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,5 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+sed -i 's/192.168.1.1/192.168.100.2/g' package/base-files/files/bin/config_generate
