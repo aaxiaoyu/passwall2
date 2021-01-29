107a0e487b92d3a0edb9160564a4bd66825d955b
z826540272 <56025847+z826540272@users.noreply.github.com>
2021-01-29 13:24:31 +0000

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,4 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed -i '/+PACKAGE_$(PKG_NAME)_INCLUDE_Xray:xray/d' feeds/passwall/luci-app-passwall/Makefile
+#sed -i '/+PACKAGE_$(PKG_NAME)_INCLUDE_Xray:xray/d' feeds/passwall/luci-app-passwall/Makefile
